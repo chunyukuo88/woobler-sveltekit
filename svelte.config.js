5c1e9659ebb808325f4000d524b273d20df36737
chunyukuo88 <alexgochenour@gmail.com>
2026-01-24 21:28:18 -0500

    feat: deployment successful

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
-import { preprocess } from 'svelte/compiler';
 
 const config = {
-	preprocess: preprocess(),
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html'
