2efd04066dabcbb665180297bd66f17352d141f7
chunyukuo88 <alexgochenour@gmail.com>
2026-01-27 00:19:41 -0500

    feat: env variables
    - Flow control
    - WIP

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,18 +1,61 @@
-export async function load() {
+import {
+	FF_PRIVATE_IMAGES_KEY,
+	FF_PRIVATE_IMAGES_VAL,
+	PRIVATE_IMAGE_SOURCE,
+	NON_PRIVATE_IMAGE_SOURCE,
+	BUCKET_MAIN_PRIVATE,
+	BUCKET_MAIN_NOT_PRIVATE
+} from '$env/static/private';
+
+export async function load({ url }) {
+	const keyFromQueryParam = url.searchParams.get(FF_PRIVATE_IMAGES_KEY);
+
+	const shouldGetPrivateImages = (keyFromQueryParam === FF_PRIVATE_IMAGES_VAL);
+	return shouldGetPrivateImages
+		? getFoldersPrivate()
+		: getFoldersPublic();
+}
+
+async function getFoldersPrivate(){
 	try {
-		const response = await fetch('https://6nxnraji72.execute-api.us-east-2.amazonaws.com/gallery');
+		const response = await fetch(PRIVATE_IMAGE_SOURCE);
 		const fetchResult = await response.json();
 		return {
 			albums: fetchResult,
+			showPrivateImages: true,
+			bucket: BUCKET_MAIN_PRIVATE,
 		};
 	} catch (error) {
-		console.error('it broke');
+		console.error('🥝 Unable to fetch private folders');
+		return {
+			albums: [],
+			showPrivateImages: false,
+			bucket: BUCKET_MAIN_PRIVATE,
+		};
+	}
+}
+async function getFoldersPublic(): Promise<FetchSuccessType> {
+	try {
+		const response = await fetch(NON_PRIVATE_IMAGE_SOURCE);
+		const fetchResult = await response.json();
+		return {
+			albums: fetchResult,
+			showPrivateImages: false,
+			bucket: BUCKET_MAIN_NOT_PRIVATE,
+		};
+	} catch (error) {
+		console.error('🐕 Unable to fetch public folders');
+		return {
+			albums: [],
+			showPrivateImages: false,
+			bucket: BUCKET_MAIN_NOT_PRIVATE,
+		};
 	}
 }
 
-type Photo = {
-	name: string;
-	friendlyName: string;
-	photos: string[];
-	captions: string[];
+
+type FetchSuccessType = {
+	albums: any;
+	showPrivateImages: boolean;
+	bucket: string;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
 	import StyledImage from '$lib/components/StyledImage.svelte';
-
 	let message = "Welcome to the Home Page!";
 	let { data } = $props();
 
-	const firstAlbum = data.albums[0];
-	console.log(firstAlbum);
-	const album = $derived(() => data.albums[0]);
+	let album = $derived(() => data.albums);
+	let bucket = $derived(() => data.bucket);
 </script>
 
 <h1>{message}</h1>
-<p>Try navigating using the links above.</p>
-
-<div class="grid">
-	{#each album().photos as url}
-		<StyledImage
-			src={`https://woobler-photos.s3.us-east-1.amazonaws.com/${url}`}
-			alt="for public viewing -- this doesn't include pictures of us"
-		/>
-	{/each}
-</div>
+{#if album().length > 0}
+	<div class="grid">
+		{#each album()[0].photos as url}
+			<StyledImage
+				src={`${bucket()}/${url}`}
+				alt="for public viewing -- this doesn't include pictures of us"
+			/>
+		{/each}
+	</div>
+{:else}
+	<div>Site is undergoing routine maintenance. Please check again tomorrow!</div>
+{/if}
