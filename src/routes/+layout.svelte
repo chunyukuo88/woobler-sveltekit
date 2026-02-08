5a9028315fbb1d0579144099460a883e540eb6a7
chunyukuo88 <alexgochenour@gmail.com>
2026-02-07 19:10:29 -0500

    refactor: more robust intersection observer for footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -34,7 +34,7 @@
 		class="woh__footer-text"
 		use:intersection={{
 			threshold: 0.5,
-			root: document.querySelector('.woh__footer-text'),
+			root: this,
 			onEnter: () => showWoobler(),
 		}}
 	>
