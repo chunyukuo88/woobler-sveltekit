1651934217fd71a414a76ff4212a589a934e4ae5
chunyukuo88 <alexgochenour@gmail.com>
2026-02-15 22:28:03 -0500

    refactor: extracted album selection util.
    - Extracted MainGallery component.

---
+++ src/lib/components/main-gallery/MainGallery.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import StyledImage from '$lib/components/image/StyledImage.svelte';
+	import type { Album } from '../../../routes/types';
+	let { bucket, selectedAlbum, isPrivate } = $props<{
+		bucket: string;
+		selectedAlbum: Album;
+		isPrivate: boolean;
+	}>()
+	let alt = isPrivate ? 'Visible to a select few.' : 'Image that anyone can see.';
+</script>
+
+{#each selectedAlbum.photos as photoUrl, index}
+	<StyledImage
+		caption={selectedAlbum.captions[index]}
+		src={`${bucket}/${photoUrl}`}
+		{alt}
+	/>
+{/each}
